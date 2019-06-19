5c432269f5667c7fab6be4eec0104d90c52bf126
rodrigoh00per <rodrigoh00per1@gmail.com>
2019-06-19 14:32:34 -0500

    marcadores se agrego

--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -14,27 +14,46 @@ class MapaPage extends StatelessWidget {
               onPressed: () {},
               icon: Icon(Icons.location_searching),
             )
-          ],
+          ],  
         ),
         body: _crearFlutterMap(scan));
   }
 
   _crearFlutterMap(ScanModel scan) {
     return FlutterMap(
-      options: MapOptions(zoom: 10, center: scan.getLatLong()),
-      layers: [_crearMapa()],
+      options: MapOptions(
+          zoom: 100, center: scan.getLatLong()), //en donde se localizara el mapa
+      layers: [_crearMapa(), _crearMarcadores(scan)],
     );
   }
 
   _crearMapa() {
     print("estoy adentro");
     return TileLayerOptions(
+        //aqui se ven las opciones para poder mostrar el mapa atraves de mapbox
         urlTemplate: 'https://api.mapbox.com/v4/'
             '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
         additionalOptions: {
           "accessToken":
               "pk.eyJ1Ijoicm9kcmlnb2gwMHBlciIsImEiOiJjangycXg1NWUwMXIxNDVvNXI2YWplaGxuIn0.Aq8QYqFvQqF0adHfFGMHsA",
           "id": "mapbox.streets"
+          //light,dark,streets,outdoors,satellite
         });
+  }
+
+  _crearMarcadores(ScanModel scan) {
+    return MarkerLayerOptions(markers: [
+      Marker(
+          height: 100,
+          width: 100,
+          point: scan.getLatLong(),
+          builder: (context) => Container(
+                child: Icon(
+                  Icons.location_on,
+                  color: Theme.of(context).primaryColor,
+                  size: 70,
+                ),
+              ))
+    ]);
   }
 }
